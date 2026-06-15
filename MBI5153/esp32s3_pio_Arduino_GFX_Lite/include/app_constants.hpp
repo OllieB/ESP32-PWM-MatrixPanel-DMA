c64abb02be01530849c6244420e47afc7b57512c
Ollie Brown <psynegy@gmail.com>
2026-06-15 02:10:30 +0100

    Update pinout for PCB compatibility

--- MBI5153/esp32s3_pio_Arduino_GFX_Lite/include/app_constants.hpp
+++ MBI5153/esp32s3_pio_Arduino_GFX_Lite/include/app_constants.hpp
@@ -58,36 +58,36 @@
   #define MBI_R4                  GPIO_NUM_47  
 
 #elif (GPIO_MAPPING == GPIO_MAPPING_PCB_DEVS3)  
-  #define ADDR_A_PIN              GPIO_NUM_4
-  #define ADDR_B_PIN              GPIO_NUM_5
-  #define ADDR_C_PIN              GPIO_NUM_16
-  #define ADDR_D_PIN              GPIO_NUM_17
-  #define ADDR_E_PIN              GPIO_NUM_18
+  #define ADDR_A_PIN              GPIO_NUM_16
+  #define ADDR_B_PIN              GPIO_NUM_17
+  #define ADDR_C_PIN              GPIO_NUM_6
+  #define ADDR_D_PIN              GPIO_NUM_42
+  #define ADDR_E_PIN              GPIO_NUM_5
 
-  #define MBI_GCLK                GPIO_NUM_6  // OE PIN IS GCLK apparently
-  #define MBI_LAT                 GPIO_NUM_7  //  data/command
-  #define MBI_DCLK                GPIO_NUM_15  // data clocking line?
-  #define MBI_SRCLK               GPIO_NUM_21   // I assume SR stands for Scan Row??  // When this is HIGH on these boards, output is disabled?
+  #define MBI_GCLK                GPIO_NUM_15  // OE PIN IS GCLK apparently
+  #define MBI_LAT                 GPIO_NUM_18  //  data/command
+  #define MBI_DCLK                GPIO_NUM_7  // data clocking line?
+  #define MBI_SRCLK               GPIO_NUM_9   // I assume SR stands for Scan Row??  // When this is HIGH on these boards, output is disabled?
 
   // First 1/4 of panel -> 20 rows
-  #define MBI_G1                  GPIO_NUM_10
-  #define MBI_B1                  GPIO_NUM_11 
-  #define MBI_R1                  GPIO_NUM_9  
+  #define MBI_G1                  GPIO_NUM_4
+  #define MBI_B1                  GPIO_NUM_40 
+  #define MBI_R1                  GPIO_NUM_41 
 
   // Second 1/4 of panel -> 20 rows
-  #define MBI_G2                  GPIO_NUM_13   
-  #define MBI_B2                  GPIO_NUM_14 
-  #define MBI_R2                  GPIO_NUM_12  
+  #define MBI_G2                  GPIO_NUM_14   
+  #define MBI_B2                  GPIO_NUM_45 
+  #define MBI_R2                  GPIO_NUM_39  
 
   // Third 1/4 of panel -> 20 rows
-  #define MBI_G3                  GPIO_NUM_41  
-  #define MBI_B3                  GPIO_NUM_40 
-  #define MBI_R3                  GPIO_NUM_42 
+  #define MBI_G3                  GPIO_NUM_47  
+  #define MBI_B3                  GPIO_NUM_12 
+  #define MBI_R3                  GPIO_NUM_13 
 
   // Forth 1/4 of panel -> 20 rows
-  #define MBI_G4                  GPIO_NUM_45    
-  #define MBI_B4                  GPIO_NUM_47 
-  #define MBI_R4                  GPIO_NUM_39
+  #define MBI_G4                  GPIO_NUM_21    
+  #define MBI_B4                  GPIO_NUM_10 
+  #define MBI_R4                  GPIO_NUM_11
 
 #endif
 
